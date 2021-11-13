a51a6e2ad4a7d334c36974cbfdac788a56895c72
PragyanKumar <81701254+PragyanKumar@users.noreply.github.com>
2021-11-13 18:30:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+
+var noseX = 0
+var noseY = 0 
+
+function preload(){
+clown_nose=loadImage("Clown_nose.jpg")
+}
+
+function setup(){
+    canvas=createCanvas(300,300)
+    canvas.center()
+    video=createCapture(VIDEO)
+    video.size(300,300)
+    video.hide()
+    posenet=ml5.poseNet(video,modelLoaded)
+    posenet.on('pose' , gotPoses)
+}
+
+function modelLoaded(){
+    console.log("modelLoaded is working")
+}
+
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results)
+        noseX=results[0].pose.nose.x
+        noseY=results[0].pose.nose.y
+        console.log('nose X = ' + noseX)
+        console.log('nose Y = ' + noseY)
+    }
+}
+
+function draw(){
+    image(video,0,0,300,300)
+    fill(255,0,0)
+    stroke(255,0,0)
+    //circle(noseX,noseY,20)
+    image(clown_nose,noseX-15,noseY-15,30,30)
+}
+
+function take_snapshot(){
+    save('clown.png')
+}
